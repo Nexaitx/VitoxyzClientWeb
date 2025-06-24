cfb31a35538cbdb17d09846caafb2d2042332660
rukhsana1827 <rukhsana1827asif@gmail.com>
2025-06-24 13:15:46 +0530

    added book-staff component

---
+++ src/app/pages/book-staff/book-staff.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-staff',
+  imports: [],
+  templateUrl: './book-staff.html',
+  styleUrl: './book-staff.scss'
+})
+export class BookStaff {
+
+}
