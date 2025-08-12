b8574b1a0bdffaf759dea1a1ac0200ec95847c78
rukhsana1827 <rukhsana1827asif@gmail.com>
2025-08-12 10:47:50 +0530

    updated view medicines

--- src/app/pages/medicines/view-medicine/view-medicine.ts
+++ src/app/pages/medicines/view-medicine/view-medicine.ts
@@ -1,15 +1,21 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { Footer } from '../../footer/footer';
 
 @Component({
   selector: 'app-view-medicine',
-  imports: [],
+  imports: [Footer],
   templateUrl: './view-medicine.html',
   styleUrl: './view-medicine.scss'
 })
 export class ViewMedicine {
-private router: Router = inject(Router);
+  private router: Router = inject(Router);
+
   goToMedicines() {
     this.router.navigate(['/dashboard/medicines']);
+  }
+
+  addToCart() {
+    this.router.navigate(['/dashboard/cart']);
   }
 }
