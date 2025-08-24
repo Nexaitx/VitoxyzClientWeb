cec83ecd4a97c72b819e331d3057d7ca56909ff9
rukhsana1827 <rukhsana1827asif@gmail.com>
2025-08-24 10:17:12 +0530

    editable field set

--- src/app/pages/profile/profile.ts
+++ src/app/pages/profile/profile.ts
@@ -10,5 +10,15 @@ import { RouterLink } from '@angular/router';
   styleUrl: './profile.scss'
 })
 export class Profile {
-  isEdit: boolean = false;
+editField: string | null = null; // which field is in edit mode
+
+  startEdit(field: string) {
+    this.editField = field;
+  }
+
+  submitEdit(field: string) {
+    // save logic here (API call, etc.)
+    console.log(`${field} updated!`);
+    this.editField = null;
+  }
 }
