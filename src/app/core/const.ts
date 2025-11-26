1fcd810753ea9fc870bafeeada2f6b7b7ed34ebf
Udaykiran649 <uday@nexaitx.com>
2025-11-26 13:41:44 +0530

    bookingstaff flow

--- src/app/core/const.ts
+++ src/app/core/const.ts
@@ -1,6 +1,6 @@
-export const API_URL = 'http://localhost:8080/api';
+//export const API_URL = 'http://localhost:8080/api';
 
-//  export const API_URL = "https://vitoxyz.com/Backend/api";
+  export const API_URL = "https://vitoxyz.com/Backend/api";
   // export const API_URL = "https://vitoxyzbackend.onrender.com/api"
 // export const API_URL = 'https://398ff2efa7fa.ngrok-free.app/api';
 
